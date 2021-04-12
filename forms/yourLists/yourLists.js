3da8ad58f9ebc37b64ad17e6cce776eddab7493e
Ethan Hall <lewisdecarolis@creighton.edu>
2021-04-12 16:02:02 -0500

    comments

--- forms/yourLists/yourLists.js
+++ forms/yourLists/yourLists.js
@@ -1,15 +1,18 @@
 let listNames = ["new", "old"]
 
 yourLists.onshow=function(){
+    // clear list and load to rdo from listNames array
     rdoLists.clear()
     for (i = 0; i < listNames.length; i++)
         rdoLists.addItem(listNames[i])
 }
 
 btnDeleteList.onclick=function(){
+    // gets selected list and deletes
     let sel = $("input[name=rdoLists]:checked").prop("value")
     listNames.splice(listNames.indexOf(sel), 1)
     
+    // reload rdo
     rdoLists.clear()
     for (i = 0; i < listNames.length; i++)
         rdoLists.addItem(listNames[i])
