c95a72cf4cf693e757ebd3287df77d8721ba3942
Riley Barry <lewisdecarolis@creighton.edu>
2021-04-14 16:36:02 -0500

    checks username in array

--- forms/Login/Login.js
+++ forms/Login/Login.js
@@ -5,22 +5,29 @@ let pw = 'Speedyrwb645'  // ***** put your database password here
 let netID = 'rwb12128'
 let databaseSchema = "375groupb3"  // put your netID here so this is your schema
 
-
 btnLogin.onclick=function(){
-    query = "SELECT DISTINCT state FROM customer ORDER BY state ASC;"
-    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + courseSchema + "&query=" + query)
-    if (req1.status == 200) { //everything worked.
-        lblWelcome.style.display = "block"  // none to hide
-        lblResult.style.display = "block"  // none to hide
-        lblResult.value = "The authentication code is " + req1.responseText
-        //1 good 0 bad
-        
+    query = "SELECT * FROM account;"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + databaseSchema + "&query=" + query)
+    results = JSON.parse(req.responseText)
+    
+    let user = inptUser.value
+    //console.log(results[0])
+    
+    if (req.status == 200) { //everything worked.
+        for (i = 0; i < results[0].length; i ++) {
+          if (user == results[0][i]) {
+            console.log('this user exists')
+            break
+          } else {
+            console.log('this user does not exists')
+          }
+        }
+        //console.log('success')
+        //console.log(results)
     } else {
         //Handle that. 
-        lblResult.style.display = "block"  // none to hide
-        lblResult.value = "Error Connection Not Made: " + req1.status + " readystate " + req1.readyState + " status text " + req1.statusText;
+        console.log('failure')
     }
 }
-
 
 //This is the login page
