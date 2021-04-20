ab342eed20ceecfe6288e0417774deb94ffef252
Ethan Hall <lewisdecarolis@creighton.edu>
2021-04-20 14:37:01 -0500

    delete list working

--- forms/yourLists/yourLists.js
+++ forms/yourLists/yourLists.js
@@ -3,10 +3,11 @@ let query = ""
 let results = []
 let pw = "BiA375.ekh"  
 let netID = "ekh74960"
+let schema = "375groupb3"
 
 yourLists.onshow=function(){        
   query = "SELECT list_name FROM lists"
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + schema + "&query=" + query)
 
   if (req.status == 200) { 
     results = JSON.parse(req.responseText)
@@ -15,24 +16,23 @@ yourLists.onshow=function(){
    else { 
        rdoLists.clear()
        for (i = 0; i < results.length; i++)
-           rdoLists.addItem(listNames[i])
+           rdoLists.addItem(results[i])
     }
-
-} else
-    console.log(`Error code: ${req.status}`)   
+  } else
+      console.log(`Error code: ${req.status}`)   
 }
 
 btnDeleteList.onclick=function(){
     let customerDelete = $("input[name=rdoLists]:checked").prop("value")
     query = "DELETE FROM lists WHERE list_name = '" + customerDelete + "'"      
-    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + schema + "&query=" + query)
     
       if (req.status == 200) {
             if (req.responseText == 500) {    
                 console.log(`You have successfully deleted the customer named ${customerDelete}`)
                 
                 query = "SELECT list_name FROM lists"
-                req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+                req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + schema + "&query=" + query)
 
                 if (req.status == 200) { 
                     results = JSON.parse(req.responseText)
@@ -41,7 +41,7 @@ btnDeleteList.onclick=function(){
                     else { 
                        rdoLists.clear()
                        for (i = 0; i < results.length; i++)
-                           rdoLists.addItem(listNames[i])
+                           rdoLists.addItem(results[i])
                     }
                 } else
                     console.log(`Error code: ${req.status}`)
