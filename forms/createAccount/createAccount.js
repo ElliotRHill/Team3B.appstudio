2dface47c5874e4e59ce923e365ff193dd1cab95
Riley Barry <lewisdecarolis@creighton.edu>
2021-04-28 16:21:05 -0500

    COMPLETE LOGIN FORMS

--- forms/createAccount/createAccount.js
+++ forms/createAccount/createAccount.js
@@ -8,6 +8,8 @@ btnCreateAcc.onclick = function() {
   resultsUser = JSON.parse(req.responseText)
 
   let newUser = inptCreateUser.value
+  let newPassOne = inptPassOne.value
+  let newPassTwo = inptPassTwo.value
 
   if (req.status == 200) { //everything worked.
     for (i = 0; i < resultsUser.length; i++) {
@@ -22,9 +24,20 @@ btnCreateAcc.onclick = function() {
       }
       //ends loop to check username
     }
-  } else {
+  }  else {
     //Handle that. 
     console.log('failure user')
   }
   
+  if ((checkUser == 0) && (newPassOne == newPassTwo)) {
+    console.log('second loop worked')
+    
+    query = "INSERT INTO account (username, password) VALUES ('" + newUser + "', '" + newPassOne + "');"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + databaseSchema + "&query=" + query)
+  }  
+  if (req.status == 200) {
+    console.log('the username and password were added')
+  } else {
+    console.log('the ajax failed')
+  }
 }
