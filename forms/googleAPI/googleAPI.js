8a2d6fb5975891a9f752c781317a8ed653f2544a
lewisdecarolis <lsd81882@creighton.edu>
2021-04-29 19:10:17 -0500

    add to database

--- forms/googleAPI/googleAPI.js
+++ forms/googleAPI/googleAPI.js
@@ -1,5 +1,5 @@
 //Array with call from Postman
-grocStores = ["Walmart: 360 Saddle Creek Rd, Omaha, NE","Hy-Vee: 2323 W Broadway, Council Bluffs, IA","ALDI: 4801 N 30th St, Omaha, NE","Family Dollar: 834 S 24th St, Omaha, NE","ALDI: 885 S 72nd St, Omaha, NE","Family Dollar: 2601 N 16th St, Omaha, NE","Hy-Vee: 7910 Cass St, Omaha, NE","Family Dollar: 1500 N 24th St, Omaha, NE","Trader Joe's: 3552 Leavenworth St, Omaha, NE"]
+grocStores = ["Walmart","Hy-Vee: Broadway","ALDI: N 30th","Family Dollar: S 24th","ALDI: S 72nd","Family Dollar: 16th","Hy-Vee: Cass","Family Dollar: N 24th","Trader Joe's"]
 
 
 
@@ -9,32 +9,42 @@ lon = [-95.9811922, -95.879364, -95.9559953, -95.94750669999999, -96.0222567, -9
 
 //API call
 // 1. *** use your own url copied from Postman ****
-let requestURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=grocery store &key=AIzaSyDoaVtASvuYckCYDer224otaiYRZApFXiw&location=41.265331,-95.949364&radius=2000&type=grocery_or_supermarket &maxprice=10"
+let requestURL2 = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=grocery store &key=AIzaSyDoaVtASvuYckCYDer224otaiYRZApFXiw&location=41.265331,-95.949364&radius=1000&type=grocery_or_supermarket"
+var Groc= []
+let apiData2 = ""
+let message2 = ""
 
-function onXHRLoad() {
-    let message = ""
+function onXHRLoad2() {
+    let message2 = ""
     
     // 'this' is another name for the object returned from the API call
-    let apiData = JSON.parse(this.responseText)
+    let apiData2 = JSON.parse(this.responseText)
     
-    for (i = 0; i <= apiData.results.length - 1; i++) {
-        console.log(`${apiData.results[i].formatted_address}`)
-        message = message + apiData.results[i].formatted_address + "\n" + apiData.results[i].name + "\n" + "\n" 
+    for (i = 0; i <= apiData2.results.length - 1; i++) {
+     Groc[i] = {  
+       "description": apiData2.results[i].name,
+       "lat":apiData2.results[i].lat,
+       "lng":apiData2.results[i].lng,
+       "address":apiData2.results[i].formatted_address
+       }
+       console.log(`${apiData2.results[i].formatted_address}`)
+        message2 = message2 + apiData2.results[i].formatted_address + "\n" + apiData2.results[i].name + "\n" + "\n" 
     }
     
+    
     // 2. *** put your textarea control name here ****
-    txtaLocation.value = message
+    txtaLocation.value = message2
     
     // if want to add to database call a function here that does that
     // addToDatabase()
 }
 
-function callAPI(URL) {
+function callAPI2(URL) {
     var xhttp = new XMLHttpRequest();
     
     // if you need cors (you'll get a cors error if you don't have it and you need it)
     // use this code to add the cors code to your url 
-    xhttp.open('GET', 'https://cors.bridged.cc/' + requestURL)
+    xhttp.open('GET', 'https://cors.bridged.cc/' + requestURL2)
     
     // if you DON'T need cors use this code:
     //xhttp.open('GET',URL)
@@ -60,7 +70,7 @@ function callAPI(URL) {
     */
 
     // make the API request
-    xhttp.addEventListener('load', onXHRLoad)
+    xhttp.addEventListener('load', onXHRLoad2)
     xhttp.send()
 }
 
@@ -69,56 +79,43 @@ function callAPI(URL) {
 
 btnLocation.onclick=function(){
    // call the code that will make the API call, then process what comes back
-    callAPI(requestURL)
+    callAPI2(requestURL2)
 }
 
 
-
-
-
+/////////////////////////////////////////////////////////////////////////////////////////
 
 //Add to select
 googleAPI.onshow=function(){
-  selLoc.clear()   
+  drpLocation.clear()   
     // put array of flavors in the dropdown (called populating it)
-    for (i = 0; i < grocStores.length; i++) 
-        selLoc.addItem(grocStores[i])
+    for (i = 0; i < myPlaces.length; i++) 
+        drpLocation.addItem(myPlaces[i].name)
+        
 }
  
 
 // multiple list choices allowed; uses button onclick
 // Comment code above, and uncomment code below
 
-btnFavLoc.onclick=function(){
-  // returns array of the choices' text
-    let message = "You chose:"
-  for (i = 0; i < selLoc.text.length; i++)
-     message = message + "\n" + selLoc.text[i] + " \n" + "\n" 
-     
-  // remove the last comma
-  // slice drops last 2 characters (comma and space)
-  //     starts at 0, and goes to end of the 
-  //     string minus 2 characters
-  message = message.slice(0, -2)
-  txtaLocation.value = message
 
 
 
-}
 
-////////////////////////////////////////////Need to add markers into select//////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
 
 myPlaces = [
-              {name: "Walmart Neighborhood Market",lat:41.2626004,lon:-95.9811922},
-              {name: "Hy-Vee",lat:41.261463,lon:-95.879364},
-              {name: "ALDI",lat:41.3024318,lon:-95.9559953},
-              {name: "Family Dollar",lat:41.2512087,lon:-95.94750669999999},
-              {name: "ALDI",lat:41.2520613,lon:-96.0222567},
-              {name: "Family Dollar",lat:41.21445370000001,lon:-95.95798689999999},
-              {name: "Family Dollar",lat:41.2831746,lon:-95.9370907},
-              {name: "Hy-Vee",lat:41.265507,lon:-96.03931999999999},
-              {name: "Trader Joe's",lat:41.2482854,lon:-96.0739732}
+              {name: "Walmart Neighborhood Market: 360 Saddle Creek Rd",lat:41.2626004,lon:-95.9811922,category: 1},
+              {name: "Hy-Vee: 2323 W Broadway, Council Bluffs",lat:41.261463,lon:-95.879364,category: 2},
+              {name: "ALDI: 4801 N 30th St",lat:41.3024318,lon:-95.9559953,category: 3},
+              {name: "Family Dollar: 834 S 24th St",lat:41.2512087,lon:-95.94750669999999,category: 4},
+              {name: "ALDI: 885 S 72nd St",lat:41.2520613,lon:-96.0222567,category: 5},
+              {name: "Family Dollar: 2601 N 16th St",lat:41.21445370000001,lon:-95.95798689999999,category: 6},
+              {name: "Family Dollar: 1500 N 24th St",lat:41.2831746,lon:-95.9370907,category: 7},
+              {name: "Hy-Vee: 7910 Cass St",lat:41.265507,lon:-96.03931999999999,category: 8},
+              {name: "Trader Joe's: 10305 Pacific St",lat:41.2482854,lon:-96.0739732,category: 9,}
             ]
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var marker
 var infowindow
@@ -135,87 +132,58 @@ function gotLocation(location, lat, long) {
     gmLocations.refresh()
 
     //Put a marker on our location
+     
     point1 = new google.maps.LatLng(location.coords.latitude, location.coords.longitude)
     marker1 = gmLocations.setMarker({
-        position: point1,
+        label: point1,
         title: "My Location"    // hover by balloon tip tooltip name
-    })
-    //All hard-coded below, but the loop takes care of it
-    /*
-   //Walmart
-    point2 = new google.maps.LatLng(41.2626004, -95.9811922);
-    marker2 = gmLocations.setMarker({
-        position: point2,
-        title: "Walmart Neighboorhood Market"
-    });
-
-    //Hyvee
-    point3 = new google.maps.LatLng(41.261463, -95.879364);
-    marker3 = gmLocations.setMarker({
-        position: point3,
-        title: "Hy-Vee"
-    });
-
-    //Family Dollar
-    //loop possibility
-    point4 = new google.maps.LatLng(41.2512087, -95.94750669999999);
-    marker4 = gmLocations.setMarker({
-        position: point4, 
-        title: "Family Dollar"
-    });
-     //Aldi
-    point5 = new google.maps.LatLng(41.3024318, -95.9559953);
-    marker5 = gmLocations.setMarker({
-        position: point5,
-        title: "Aldi" 
-        });
-        
-         //ALDI
-    point6 = new google.maps.LatLng(41.2520613, -96.0222567);
-    marker6 = gmLocations.setMarker({
-        position: point6,
-        title: "Aldi"
-        });
-          //"Family Dollar"
-    point7 = new google.maps.LatLng(41.21445370000001, -95.95798689999999);
-    marker7 = gmLocations.setMarker({
-        position: point7,
-        title: "Family Dollar"
-        });
-          //"Family Dollar"
-    point8 = new google.maps.LatLng(41.2831746, -95.9370907);
-    marker8 = gmLocations.setMarker({
-        position: point8,
-        title: "Family Dollar"
-        });
-          //, "Hy-Vee"
-    point9 = new google.maps.LatLng(41.265507, -96.03931999999999);
-    marker9 = gmLocations.setMarker({
-        position: point9,
-        title: "Hy-Vee"
-        });
-          //, "Trader Joe's
-    point10 = new google.maps.LatLng(41.2482854, -96.0739732);
-    marker10 = gmLocations.setMarker({
-        position: point10,
-        title: "Trader Joe's"
-      });
-*/
-
-//loop of myPlaces to put markers on
-
+        })
+  const infowindow = new google.maps.InfoWindow({
+    content: "My Location"
+  })     
+ marker1.addListener("click", () => {
+    infowindow.open(gmLocations, marker1);
+  })
+  
+  
+}
     
-    let tempPoint = ""
-    let tempMarker = ""
-    for (i = 0; i < myPlaces.length;i++) {
-      tempPoint = new google.maps.LatLng(myPlaces[i].lat,myPlaces[i].lon)
+    //Dropdown to choose location
+  
+ 
+drpLocation.onclick = function(s){
+//loop of myPlaces to put markers on
+   let tempPoint = ""
+    let tempMarker = ""  
+   
+   for (i = 0; i < myPlaces.length;i++) {
+       
+       if (typeof(s) == "object")  {
+            return
+      
+      } else if (drpLocation.selection == myPlaces[i].name){
+         
+           tempPoint = new google.maps.LatLng(myPlaces[i].lat,myPlaces[i].lon)
       tempMarker = gmLocations.setMarker({
         position: tempPoint,
-        title: grocStores[i]
-      })
-    }
+        title: myPlaces[i].name
+     }) 
+    
+    const infowindow = new google.maps.InfoWindow({
+    content: myPlaces[i].name
+  })     
+ 
+ tempMarker.addListener("click", () => {
+    infowindow.open(gmLocations, tempMarker);
+  });
+        
 }
+}  
+    }
 
+   
+   
+   
 
 btnCL4.onclick = function() {
     // have to run this before you do anything else - call this getLocation button
@@ -224,6 +192,44 @@ btnCL4.onclick = function() {
 }
 
 
-btnBackMenu.onclick=function(){
-  ChangeForm(menu)
+
+
+
+btnClear.onclick=function(){
+  gmLocations.refresh()
 }
+
+
+
+
+
+
+
+
+hmbMenu3.onclick=function(s){
+    if (typeof(s) == "object") {
+       return
+    } else {
+       switch(s) {
+        case "Home":
+            ChangeForm(yourLists)
+            break;
+        case "View Lists":
+            ChangeForm(yourLists)
+            break;
+        case "Add to Current List":
+            ChangeForm(newList)
+            break;            
+        case "Nearby Stores":
+            ChangeForm(googleAPI)
+            break;
+        case "Weather":
+            ChangeForm(weather)
+            break;
+        case "Recipe":
+            ChangeForm(recipeAPI)
+            break;
+        }
+    }  
+}
+
